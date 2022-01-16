09300420197d32003ca48be2bd29d520cd8707e4
Freddyblitz <fynn.schur@web.de>
2022-01-16 21:14:21 +0100

    create package structure; add main class

---
+++ src/main/java/de/fynn/sco/core/SCOCore.java
@@ -0,0 +1,17 @@
+package de.fynn.sco.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SCOCore extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
